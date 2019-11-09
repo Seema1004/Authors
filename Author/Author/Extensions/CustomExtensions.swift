717ad25f84a0a68aab0c7abcc84feeab119d1aa6
Seema Sharma <Seema1004@gmail.com>
2019-11-10 02:11:50 +0530

    added comments

--- Author/Author/Extensions/CustomExtensions.swift
+++ Author/Author/Extensions/CustomExtensions.swift
@@ -10,6 +10,8 @@ import UIKit
 
 extension String {
 
+    //To display a readable format date to the User
+    
     func getFormattedDate() -> String?{
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
